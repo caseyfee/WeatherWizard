3e493fd4ea88b5f6653bcc9b5921ced8e7466d89
caseyfee <caseyfee3@gmail.com>
2023-01-31 10:39:29 -0800

    layout without bootstrap

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// var APIKey = 029f73215f94df358a06425c3bef0fed
+var APIKey = "029f73215f94df358a06425c3bef0fed";
 // https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
 
 var city;
 var state;
