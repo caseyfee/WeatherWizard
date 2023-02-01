17a10afd67cb0d3cdd6a6dc6f4ca9daf2332cb8f
caseyfee <caseyfee3@gmail.com>
2023-01-31 18:38:44 -0800

    API connected but erroring

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-var APIKey = "029f73215f94df358a06425c3bef0fed";
+
 // https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
-
-var city;
+var APIKey = "029f73215f94df358a06425c3bef0fed";
+var city = "chicago";
 var state;
 var county;
 var zipCode;
+var queryURL = "api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
+
+fetch(queryURL)
+    .then(function (response) {
+    return response.json();
+    console.log(response.status);
+    })
+    .then(function (data) {
+        console.log(data);
+    });
