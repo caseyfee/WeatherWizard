a9044d9c330416a2fbc549ebd1c10759370ae0ab
caseyfee <caseyfee3@gmail.com>
2023-02-02 18:32:07 -0800

    pulling 5 day forecast API

--- script.js
+++ script.js
@@ -8,15 +8,28 @@ var zipCodeQuery ="https://api.openweathermap.org/geo/1.0/zip?zip="+zipCode+"&ap
 var limit = 5;
 var extraCityQueryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
 var cityQueryURL = "https://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit="+limit+"&appid="+APIKey;
+var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey;
 
 // https://api.openweathermap.org/geo/1.0/direct?q=chicago&limit=5&appid=029f73215f94df358a06425c3bef0fed
 
-var lat;
-var lon;
+// Create lat and long dynamically by taking from the first API pull to make sure 5 day forecast is pulling correct city
+var lat = "41.85";
+var lon = "-87.65";
 
 // var latlongQueryURL = "api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey;
 
-fetch(cityQueryURL)
+// Create an action to pull the city data from the input
+
+
+// var mainSearchInput = function(event){
+//     event.preventDefault();
+//     // Prevent Default
+
+//     var mainSearchEL=event.target.getElementbyId('search-addon');
+    
+//     console.log(event.target)
+
+    fetch(cityQueryURL)
     .then(function (response) {
         console.log(response.status);
         if (response.status !==200){
@@ -26,5 +39,36 @@ fetch(cityQueryURL)
     })
     .then(function (data) {
         console.log(data);
-        for 
+
+        // Go through array and chose the first city suggested 
+
+        // for (var i = 0; i < 1; i++) {
+            // var userName = document.createElement('h3');
+            // var issueTitle = document.createElement('p');
+            // userName.textContent = data[i].user.login;
+            // issueTitle.textContent = data[i].title;
+            // issueContainer.append(userName);
+            // issueContainer.append(issueTitle);
+        // }
     });
+// }
+
+
+// Pull another API with the 5 day forcast
+
+fetch("https://api.openweathermap.org/data/2.5/forecast?lat=41.85&lon=-87.65&appid=029f73215f94df358a06425c3bef0fed")
+    .then(function (response) {
+        console.log(response.status);
+        if (response.status !==200){
+            console.log("CHECK NETWORK")
+        }
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        // Connect data with index.html location where it will be presented
+        // 
+    });
+
+    // Save the search events by creating keys for each datapoints that are pulled and save them locally
+    // mainSearchEl.addEventListener('click', mainSearchInput);
