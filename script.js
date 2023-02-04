2ce80762be6d3f7de04512f0f5dd063872ab697e
caseyfee <caseyfee3@gmail.com>
2023-02-03 17:51:00 -0800

    Created API pull functions (Erroring- pretutoring)

--- script.js
+++ script.js
@@ -8,31 +8,42 @@ var zipCodeQuery ="https://api.openweathermap.org/geo/1.0/zip?zip="+zipCode+"&ap
 var limit = 5;
 var extraCityQueryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
 var cityQueryURL = "https://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit="+limit+"&appid="+APIKey;
-var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey;
 
 // Dom Elements
+// Create an element that takes in the user input
 
-
+var cityInputEl = document.getElementById("cityName");
+var searchContainerEl = document.querySelector('#input-group');
 
 
 // https://api.openweathermap.org/geo/1.0/direct?q=chicago&limit=5&appid=029f73215f94df358a06425c3bef0fed
 
-// Create lat and long dynamically by taking from the first API pull to make sure 5 day forecast is pulling correct city
-var lat = "41.85";
-var lon = "-87.65";
 
-// var latlongQueryURL = "api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey;
+
 
 // Create an action to pull the city data from the input
 
 
-// var mainSearchInput = function(event){
-//     event.preventDefault();
-//     // Prevent Default
+var mainSearchInput = function(event){
+    event.preventDefault();
+   
+    var city=cityInputEl.value.trim();
+    
+    if (city) {
+        getCityInfo(city);
 
-//     var mainSearchEL=event.target.getElementbyId('search-addon');
+        searchContainerEl.textContent = '';
+        cityInputEl.value='';
+    }
+    else {
+        console.log("there is an issue");
+    }
+
+    
     
 //     console.log(event.target)
+
+var getCityInfo = function(city){
 
     fetch(cityQueryURL)
     .then(function (response) {
@@ -44,36 +55,39 @@ var lon = "-87.65";
     })
     .then(function (data) {
         console.log(data);
+// Created lat and long dynamically by taking from the first API pull to make sure 5 day forecast is pulling correct city
 
-        // Go through array and chose the first city suggested 
+        // console.log(data [1].lat);
+        var cityLat = data[1].lat;
+        var cityLon = data[1].lon;
+        
+        // Pull another API for 5 day forecast using the city Lat and Lon
+        var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+cityLat+"&lon="+cityLon+"&appid="+APIKey;
+        fetch(fiveDayQueryURL)
+        .then(function (fiveDayResponse){
+            if(fiveDayResponse.ok) {
+            return fiveDayResponse.json();
+            // var fiveDayData = "";
+            .then(function(fiveDayData)) {
+                console.log(fiveDayData);
 
-        // for (var i = 0; i < 1; i++) {
-            // var userName = document.createElement('h3');
-            // var issueTitle = document.createElement('p');
-            // userName.textContent = data[i].user.login;
-            // issueTitle.textContent = data[i].title;
-            // issueContainer.append(userName);
-            // issueContainer.append(issueTitle);
-        // }
-    });
+            }}
+            }
+            
+            )
+        })};
+    
 // }
 
+        cityLat = data[1].lat;
+        cityLon = data[1].lon;
+        
+        
+        // https://api.openweathermap.org/data/2.5/forecast?lat=41.85&lon=-87.65&appid=029f73215f94df358a06425c3bef0fed
 
-// Pull another API with the 5 day forcast
+    //  Create dom variables to connect with index file and start populating current data
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat=41.85&lon=-87.65&appid=029f73215f94df358a06425c3bef0fed")
-    .then(function (response) {
-        console.log(response.status);
-        if (response.status !==200){
-            console.log("CHECK NETWORK")
-        }
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        // Connect data with index.html location where it will be presented
-        // 
-    });
+
 
     // Save the search events by creating keys for each datapoints that are pulled and save them locally
     // mainSearchEl.addEventListener('click', mainSearchInput);
