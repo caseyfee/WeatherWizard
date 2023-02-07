9879b841dd303ecf14cdbe3df3c470161b450cee
caseyfee <caseyfee3@gmail.com>
2023-02-07 11:02:22 -0800

    icon working, date and search troublesho

--- script.js
+++ script.js
@@ -34,9 +34,10 @@ var mainSearchInput = function(){
 
   var showCurrentWeather = function(cityName, data) {
     var currentWeatherHTML = `<h4 class="currentCity mb-1 sfw-normal">${cityName}</h4>
-    <p class="mb-2">Current temperature: <strong>${Number(data.main.temp-273.15).toFixed(1)}°C</strong></p>
-    <p>Feels like: <strong>${Number(data.main.feels_like-273.15).toFixed(1)}°C</strong></p>
-    <p>Max: <strong>${Number(data.main.temp_max-273.15).toFixed(1)}°C</strong>, Min: <strong>${Number(data.main.temp_min-273.15).toFixed(1)}°C</strong></p>
+    <h5 mb-1 sfw-normal">${date}</h5>
+    <p class="mb-2">Current temperature: <strong>${convertTemp(Number(data.main.temp).toFixed(1))}°F</strong></p>
+    <p>Feels like: <strong>${convertTemp(Number(data.main.feels_like).toFixed(1))}°F</strong></p>
+    <p>Max: <strong>${convertTemp(Number(data.main.temp_max).toFixed(1))}°F</strong>, Min: <strong>${convertTemp(Number(data.main.temp_min).toFixed(1))}°F</strong></p>
 
     <div class="d-flex flex-row align-items-center">
         <p class="mb-0 me-4">${data.weather[0].description}</p>
@@ -44,6 +45,28 @@ var mainSearchInput = function(){
     </div>`;
 
     $('#currentCityWeather').html(currentWeatherHTML)
+
+    function convertTemp(k) {
+            return Math.floor((k - 273.15) * 1.8 + 32);
+        }
+
+        var date = "";
+        // Get Today's Date
+        const unixTime = `${data.dt}`;
+        const milliseconds = unixTime*1000;
+        const dateobject = new Date(milliseconds);
+
+        const newDateFormat = dateobject.toLocaleString();
+        console.log(newDateFormat);
+        
+        date = newDateFormat;
+        console.log(typeof(date));
+
+        // const currentDay = new Date(data.dt);
+        //         const day = currentDay.getDate() - 1;
+        //         const month = currentDay.getMonth() + 1;
+        //         const year = currentDay.getFullYear();
+        //         date.textContent = month + "/" + day + "/" + year;
 
   };
 
@@ -70,7 +93,7 @@ var getCityInfo = function (city) {
             var cityLon = data.coord.lon;
             fiveDayInfo(cityLat, cityLon);
             showCurrentWeather(city, data);
-            // getIcons (data);
+            displayHistory(city);
         })
 };
 
@@ -97,59 +120,74 @@ var fiveDayInfo = function (cityLat, cityLon) {
                     dataArray.push(fiveDayData.list[i]);
                     console.log(dataArray);   
                         
-                    }
+                    }        
             
-            // dataArray = fiveDayData.list.filter((fiveDates)=>{fivedate%8===0});
-            // console.log(dataArray);
-        } show5Day (city, dataArray);
+        } show5Day (dataArray);
     }) 
     } 
 
-// function convertUTCDateToLocalDate(date) {
-//         var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
     
-//         var offset = date.getTimezoneOffset() / 60;
-//         var hours = date.getHours();
-    
-//         newDate.setHours(hours - offset);
-    
-//         return newDate;   
-//     }
 
 
-function show5Day(city, dataArray){
+
+function show5Day(dataArray){
     var fivedayHTML = ``;
     for(var i=0;i<dataArray.length;i++){
        fivedayHTML += `
         <div class="forecast d-flex justify-content-around" id="fiveDayContainer">
                 <div class="card shadow-0 border">
                     <div class="card-body p-4 bg-info mb-3" id="Day1">
-                        <h4 class="currentCity mb-1 sfw-normal">${city}</h4>
-                        <p class="mb-2 dayOneTemp">Current temperature: <strong>${Number(dataArray[i].main.temp-273.15).toFixed(1)}°C</strong></p>
+                        <h4 class="currentCity mb-1 sfw-normal">${dataArray[i].dt}</h4>
+                        <p class="mb-2 dayOneTemp">Current temperature: <strong>${convertTemp(Number(dataArray[i].main.temp).toFixed(1))}°C</strong></p>
                         <p class="dayOneWind">Wind Speed: <strong>${Number(dataArray[i].wind.speed).toFixed(1)} mph</strong></p>
                         <p class="dayOneHumid">Humidity: <strong>${Number(dataArray[i].main.humidity).toFixed(1)}%</strong></p>
 
                         <div class="d-flex flex-row align-items-center">
                             <p class="mb-0 me-4">${dataArray[i].weather[0].description}</p>
-                            <img class="fas fa-cloud fa-3x" style="color: #eee;" scr ="http://openweathermap.org/img/wn/${dataArray[i].weather[0].icon}.png">
+                            <img class="fas fa-cloud fa-3x" style="color: #eee;" src = "http://openweathermap.org/img/wn/${dataArray[i].weather[0].icon}.png">
                         </div>
                     </div>
                 </div>
                 </div>
             </div>
         `
-    
+        function convertTemp(k) {
+            return Math.floor((k - 273.15) * 1.8 + 32);
+        }
+
+        //  // Adding the dates in the 5 day 
+        //  const currentDay = new Date(data.list[0].dt_txt);
+        //  const day = currentDay.getDate() - 1;
+        //  const month = currentDay.getMonth() + 1;
+        //  const year = currentDay.getFullYear();
+        //  date.textContent = month + "/" + day + "/" + year;
+
                                                                                 
     } $('#fiveDayContainer').html(fivedayHTML);
     }
+
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+// Saving Searches
+// Save the search events by creating keys for each datapoints that are pulled and save them locally
+    function displayHistory(city) {
+        var previousSearchesHTML = ``;
+
+        for (i = 0; i < searchHistory.length; i++) {
+            previousSearchesHTML += `
+            <a type="button">
+            <span class="input-group-text border-0 fw-bold" >
+                ${city[i]}
+            </span>
+             </a>`
+
+            historyEl.addEventListener("click", function () {
+                getCityInfo(city);
+            })
+
+        } $('#previousCities').html(previousSearchesHTML);
+    }
+
+   
+
     
-    // var fiveDayTemp = main.temp;
-    // var fiveDayWind = weather.main;
-    // var fiveDayHumid = main.humidity;
-    // var fiveDayIcon = weather.icon;
-    // $('#fiveDays').html(fivedayHTML)
-
-    // Saving Searches
-
-    // Save the search events by creating keys for each datapoints that are pulled and save them locally
-    // mainSearchEl.addEventListener('click', mainSearchInput);
